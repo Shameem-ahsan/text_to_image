d65509d7534c30a1e469c958d2cdb352b162b110
shameem <glowdemoerp@gmail.com>
2019-09-04 17:18:38 +0530

    Migrate To Androidx

--- app/src/main/java/meridian/cybraum/com/texttoimage/MainActivity.java
+++ app/src/main/java/meridian/cybraum/com/texttoimage/MainActivity.java
@@ -4,17 +4,13 @@ import java.util.Calendar;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
+import androidx.core.app.ActivityCompat;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
